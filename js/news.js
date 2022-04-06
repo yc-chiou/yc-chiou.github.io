ada8f707d56a1881a8fa0b217d714f8b85903033
Yu-Cheng Chiou <56761191+yc-chiou@users.noreply.github.com>
2022-04-06 23:26:55 +0800

    Update news.js

--- js/news.js
+++ js/news.js
@@ -1,10 +1,1 @@
-const news = [
-  {
-    title: "Paper accepted @ DAC 2022",
-    image_src: "images/2022_DAC_logo.png",
-    image_url: "https://www.dac.com",
-    content: `My paper "Efficient Timing Propagation with Simultaneous Structural and Pipeline Parallelisms" is accepted by DAC 2022.`,
-    footer: "Last updated on 2022/04/05"
-  }
-]
 
