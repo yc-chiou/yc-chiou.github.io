30db1ecbec63474ee990f5f0121308f35a78710f
Yu-Cheng Chiou <56761191+yc-chiou@users.noreply.github.com>
2022-04-06 21:58:54 +0800

    Add files via upload

---
+++ js/news.js
@@ -0,0 +1,82 @@
+const news = [
+  {
+    title: "Paper accepted @ DAC 2022",
+    image_src: "images/2022_DAC_logo.png",
+    image_url: "https://www.dac.com",
+    content: `My paper "Efficient Timing Propagation with Simultaneous Structural and Pipeline Parallelisms" is accepted by DAC 2022.`,
+    footer: "Last updated on 2022/04/05"
+  },
+
+  {
+    title: "Reviewer @ DAC 2022",
+    image_src: "images/2022_DAC_logo.png",
+    image_url: "https://www.dac.com",
+    content: `I reviewed papers of DAC2022.`,
+    footer: "Last updated on 2022/01/25"
+  },
+
+  {
+    title: "Attendance @ DAC Young Fellows Program 2021",
+    image_src: "images/2021_DAC_logo.png",
+    image_url: "https://www.dac.com/Attend/Students-Scholarships/Young-Student-Fellow-Program",
+    content: `I attended DAC Young Fellows Program 2021. The Design Automation Conference (DAC) is recognized as the premier conference for design and automation of electronic systems.`,
+    footer: "Last updated on 2021/11/15"
+  },
+  {
+    title: "Attendance @ CPP Conference 2021",
+    image_src: "images/2021-cppcon-banner.jpg",
+    image_url: "https://cppcon.org",
+    content: `I attended CPP Conference 2021 on October 24-29 in Aurora, Colorado`,
+    footer: "Last updated on 2021/09/30"
+  },
+  {
+    title: "Attendance @ ERUO PAR 2021",
+    image_src: "images/2021-europar.png",
+    image_url: "https://amte2021.stellar-group.org/program",
+    content: `I gave a presentation of my publication at AMTE, held in conjunction with Euro-par 2021. The title of the publication is "An Experimental Study of SYCL Task Graph Paralleliem for Large-Scale machine Learning Workloads"`,
+    footer: "Last updated on 2021/09/30"
+  },
+  {
+    title: "Attendance @ ISPD 2021 Contest",
+    image_src: "images/ISPD_21_Honorable_Mention.png",
+    image_url: "https://cerebras.net/ispd-2121-contest",
+    content: `I attended the contest which aims to map a 3-D Finite Element model on a 20D grid of processing elements in a supercomputer, Cerebras CS-1.`,
+    footer: "Last updated on 2021/03/26"
+  },
+  {
+    title: "Attendance @ ICCAD 2020",
+    image_src: "images/2020_ICCAD-logo.jpg",
+    image_url: "https://iccad.com/",
+    content: `I attended the tutorial session talking about experience of using Taskflow to parallelize CAD<q><a href="https://iccad.com/event_details?id=305-12-D">GPU Acceleration in CAD: Opportunities and Challenges</a></q> at the 2020 IEEE/ACM International Conference on Computer-aided Design (ICCAD)!`,
+    footer: "Last updated on 2021/03/26"
+  },
+  {
+    title: "Attendance @ WOSET 2020",
+    image_src: "images/woset20.png",
+    image_url: "https://woset-workshop.github.io/",
+    content: `I attended the presentation of Taskflow at the 2020 Workshop of Open-source EDA Technology (WOSET) co-located with the International Conference on Computer-aided Design (ICCAD)!`,
+    footer: "Last updated on 2021/03/26"
+  }, 
+  {
+    title: "Attendance @ MUC++ 2020",
+    image_src: "images/mucpp-meetup.jpeg",
+    image_url: "https://www.meetup.com/MUCplusplus/events/273223910/",
+    content: `I attended the event presenting to the C++ group at München, Germany. <q><a href="https://www.meetup.com/MUCplusplus/events/273223910/">Taskflow: A General-purpose Parallel and Heterogeneous Task Programming System</a></q>, on Thu, Oct 22, 7:00 PM GMT+2.`,
+    footer: "Last updated on 2021/03/26"
+  },
+  {
+    title: "Attendance @ CppCon 2020",
+    image_src: "images/cppcon.png",
+    image_url: "https://cppcon.org/",
+    content: `I attended CppCon 2020 and listened to many marvelous talks!`,
+    footer: "Last updated on 2020/10/21"
+  },
+  {
+    title: "Attendance @ HPEC 2020",
+    image_src: "images/2020_hpec.png",
+    image_url: "http://www.ieee-hpec.org/",
+    content: `I attended IEEE HPEC 2020.`,
+    footer: "Last updated on 2020/10/21"
+  }
+]
+
